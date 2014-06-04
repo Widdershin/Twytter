a30d4a276ab0c48171a3d308db26d442e8d01319
Kendall Flutey & Richard Wood <pair+kendallflutey.rwoodnz@devbootcamp.com>
2014-06-05 11:40:33 +1200

    Profile view and controllers

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,6 +4,20 @@ get '/' do
 end
 
 get '/profile' do
+  if session['user_id'] != nil
+    user = User.find_by_id(session['user_id'])
+    @username = user.username
+    @twytlist = Tweet.where(id: session['user_id'])
+    @followers = User.followers
+    @followed_users = User.followed_users
+    erb :profile
+  else
+    redirect('/')
+  end
+end
+
+post '/profile' do
+
 end
 
 get '/profile/:id' do
