4c3ea78f01d0e8653d9f0ca9c3d4753c1b92894c
Nick Johnstone & Richard Wood <pair+Widdershin.rwoodnz@devbootcamp.com>
2014-06-06 16:00:08 +1200

    Added gravatar to profile page

--- config/database.rb
+++ config/database.rb
@@ -1,6 +1,6 @@
 # Log queries to STDOUT in development
 if Sinatra::Application.development?
-  ActiveRecord::Base.logger = Logger.new(STDOUT)
+  # ActiveRecord::Base.logger = Logger.new(STDOUT)
 end
 
 # Automatically load every file in APP_ROOT/app/models/*.rb, e.g.,
