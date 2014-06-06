81a75151b4a17432191c89468afbb08059864fe5
Kendall Flutey & Tim Robinson <pair+kendallflutey.timrobinson88@devbootcamp.com>
2014-06-06 16:51:40 +1200

    Tidying up, add some stuff

--- app/helpers/twyts.rb
+++ app/helpers/twyts.rb
@@ -13,13 +13,18 @@ helpers do
         <div class = 'retweet'>
           <form action='/twyt' method='post'>
           <input type='hidden' name='twyt' value='RT @#{ twyt.user }: #{ twyt.message }'>
+          <input class="btn" id="blue" type='submit' value='Retweet'>
           </form>
-          <input type='submit' value='Retweet'>
+
+          <form action='/favourite' method='post'>
+          <input type='hidden' name='twyt_id' value='#{ twyt.id }'>
+          <input class="btn" id="blue" type='submit' value='Favourite'>
+
+          </form>
         </div>
       </div>
     <div class = 'tweetmessage'>
       <p>Message: #{linkify_usernames(twyt.message)}</p>
-
     </div>
 </div>
   HTML
