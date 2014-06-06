21edc682fbcd4b5b719950d2d43814c8f47096b7
Kendall Flutey & Tim Robinson <pair+kendallflutey.timrobinson88@devbootcamp.com>
2014-06-06 16:46:30 +1200

    fixing nicks extraneious stuff

--- app/helpers/twyts.rb
+++ app/helpers/twyts.rb
@@ -8,17 +8,17 @@ helpers do
   <div class = 'feedtweet'>
       <div class = 'tweethead'>
         <div class = 'tweetuser'>
-         From: <%= twyt.user.profile_link %>
+         From: #{ twyt.user.profile_link }
         </div>
         <div class = 'retweet'>
           <form action='/twyt' method='post'>
-          <input type='hidden' name='twyt' value='RT @<%= twyt.user %>: <%= twyt.message %>'>
+          <input type='hidden' name='twyt' value='RT @#{ twyt.user }: #{ twyt.message }'>
           </form>
           <input type='submit' value='Retweet'>
         </div>
       </div>
     <div class = 'tweetmessage'>
-      <p>Message: <%=linkify_usernames(twyt.message)%></p>
+      <p>Message: #{linkify_usernames(twyt.message)}</p>
 
     </div>
 </div>
